89f166e6008513e7394d832a13fb065a03874ba9
Julian Columbres <julian.columbres@gmail.com>
2021-02-20 17:47:09 -0800

    Added Flix Part 2 Features

--- Flix/MoviesViewController.swift
+++ Flix/MoviesViewController.swift
@@ -30,12 +30,12 @@ class MoviesViewController: UIViewController, UITableViewDataSource, UITableView
            if let error = error {
               print(error.localizedDescription)
            } else if let data = data {
+            print("ok")
               let dataDictionary = try! JSONSerialization.jsonObject(with: data, options: []) as! [String: Any]
             
               self.movies = dataDictionary["results"] as! [[String:Any]]
               
               self.tableView.reloadData()
-              print(dataDictionary)
               // TODO: Get the array of movies
               // TODO: Store the movies in a property to use elsewhere
               // TODO: Reload your table view data
@@ -68,14 +68,23 @@ class MoviesViewController: UIViewController, UITableViewDataSource, UITableView
     }
     
 
-    /*
+    
     // MARK: - Navigation
 
     // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         // Get the new view controller using segue.destination.
         // Pass the selected object to the new view controller.
+    
+        let cell = sender as! UITableViewCell
+        let indexPath = tableView.indexPath(for: cell)!
+        let movie = movies[indexPath.row]
+        
+        let detailsViewController = segue.destination as! MovieDetailsViewController
+        detailsViewController.movie = movie
+        
+        tableView.deselectRow(at: indexPath, animated: true)
+        
     }
-    */
 
 }
